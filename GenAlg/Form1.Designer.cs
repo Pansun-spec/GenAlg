f9a49116d0c2bce1793b66445dc564b01a7a4a6b
Pansun <chkehua@gmail.com>
2020-12-24 19:18:06 +0700

    添加项目文件。

---
+++ GenAlg/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace GenAlg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(132, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Размер популяции";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(132, 75);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Вер-ть скрещивания";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(348, 49);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(256, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Вер-ть мутации";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(348, 75);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(274, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Кол-во эпох";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(70, 418);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Функция:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(132, 445);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(278, 20);
+            this.textBox5.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 448);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Точка минимума:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 151);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(178, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "График функции и точек решения";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(11, 171);
+            this.chart1.Name = "chart1";
+            series3.BorderWidth = 3;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Color = System.Drawing.Color.ForestGreen;
+            series3.Legend = "Legend1";
+            series3.Name = "f(x)";
+            series4.BorderWidth = 2;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series4.Color = System.Drawing.Color.Red;
+            series4.Legend = "Legend1";
+            series4.Name = "gmin";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(484, 233);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(422, 427);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Выход";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(280, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 42);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Найти минимум";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(48, 110);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(89, 20);
+            this.textBox6.TabIndex = 0;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(143, 113);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 13);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Xmin";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 113);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(33, 13);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Xmax";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(175, 110);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(89, 20);
+            this.textBox7.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(16, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(248, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "По-умолчанию целочисленное кодирование";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 475);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Минимум функции";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ GenAlg/Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace GenAlg
+{
+    public partial class Form1 : Form
+    {
+        static Random rd;
+        static int popsize, epoch;
+        static double pcross, pmut, xmin, xmax;
+        const int lc = 10; //размер хромосомы
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //задаем начальные значения
+            textBox1.Text = "100";
+            textBox2.Text = "0.7";
+            textBox3.Text = "0.1";
+            textBox4.Text = "10";
+            textBox6.Text = "5";
+            textBox7.Text = "-5";
+            label5.Text = "Функция: f(x) = x^2 + 4;";
+            checkBox1.Checked = true; //по-умолчанию целочисленное кодирование
+
+            //инициализация переменных
+            rd = new Random(Guid.NewGuid().GetHashCode());
+            popsize = int.Parse(textBox1.Text);
+            pcross = GetContolText(textBox2);
+            pmut = GetContolText(textBox3);
+            epoch = int.Parse(textBox4.Text);
+            xmax = GetContolText(textBox6);
+            xmin = GetContolText(textBox7);
+        }
+
+        //исходная функция
+        private static double f(double x)
+        {
+            return x * x + 4.0;
+        }
+
+        //выход из приложения
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //класс особи (целочисленное кодирование)
+        public class Indv
+        {
+            public Boolean[] chrom; //хромосома
+            public double fit; //значение фитнесс функции
+            public double fen; //фенотип
+
+            public Indv(bool check) //размер хромосомы
+            {
+                chrom = new Boolean[lc];
+                for (int i = 0; i < lc; i++)
+                {
+                    chrom[i] = coin();
+                }
+                if (check) //для целочисленного кодирования
+                {
+                    fen = decode(chrom);
+                }
+                else //для вещественного
+                {
+                    fen = rd.Next() % (xmax - xmin) + xmin;
+                }
+                fit = f(fen);
+            }
+
+            public Indv DeepCopy()
+            {
+                Indv other = (Indv)this.MemberwiseClone();
+                other.chrom = new Boolean[lc];
+                Array.Copy(chrom, other.chrom, lc);
+                other.fen = decode(other.chrom);
+                other.fit = f(other.fen);
+                return other;
+            }
+        }
+
+        //бросок монеты (случайность)
+        private static Boolean coin(double p = 0.5)
+        {
+            if (p == 1)
+            {
+                return true;
+            }
+            else
+                return rd.NextDouble() <= p;
+        }
+
+        //декодирование хромосомы
+        private static double decode(Boolean[] chrom)
+        {
+            double ac = 0;
+            double pw = 1;
+            for (int i = 0; i < chrom.Length; i++)
+            {
+                if (chrom[i])
+                {
+                    ac += pw;
+                }
+                pw *= 2;
+            }
+            return xmin + (xmax - xmin) * ac / (pw - 1);
+        }
+
+        //Отбор
+        private static void Shuffle(Indv[] pop)
+        {
+            for (int i = pop.Length - 1; i > 0; i--)
+            {
+                int randomIndex = rd.Next(0, i + 1);
+                Indv temp = pop[i].DeepCopy();
+                pop[i] = pop[randomIndex].DeepCopy();
+                pop[randomIndex] = temp.DeepCopy();
+            }
+        }
+
+        //турнир с k = 2
+        private static void Tour(Indv[] pop, ref int pick, ref int idx)
+        {
+            if (pick >= pop.Length)
+            {
+                Shuffle(pop); //перемешиваем в случайном порядке
+                pick = 0;
+            }
+
+            int i1 = pick;
+            int i2 = pick + 1;
+
+            //тут можно поменять знак в зависимости от задачи
+            pick += 2;
+            if (pop[i1].fit < pop[i2].fit)
+            {
+                idx = i1;
+            }
+            else
+            {
+                idx = i2;
+            }
+        }
+
+        //отбираем популяцию
+        private static Indv[] Selection(Indv[] pop)
+        {
+            int pick = 0;
+            int idx = 0;
+            List<Indv> nwpop = new List<Indv>();
+            for (int i = 0; i < pop.Length; i++)
+            {
+                Tour(pop, ref pick, ref idx);
+                nwpop.Add(pop[idx].DeepCopy());
+            }
+            return nwpop.ToArray();
+        }
+
+        //Скрещивание
+        private static void Crossover(Boolean[] c1, Boolean[] c2, Boolean[] p1, Boolean[] p2)
+        {
+            int k = 0;
+            if (coin(pcross))
+            {
+                k = rd.Next(c1.Length);
+            }
+            else
+            {
+                k = c1.Length - 1;
+            }
+
+            for (int i = 0; i < k; i++)
+            {
+                c1[i] = p1[i];
+                c2[i] = p2[i];
+            }
+            for (int i = k; i < c1.Length; i++)
+            {
+                c1[i] = p2[i];
+                c2[i] = p1[i];
+            }
+        }
+
+        //Мутация
+        private static void Mutation(Boolean[] chrom)
+        {
+            if (coin(pmut))
+            {
+                int k = rd.Next(chrom.Length);
+                chrom[k] = !chrom[k];
+            }
+        }
+
+        //арифметический кроссинговер
+        private static double Crossover(double fenp1,double fenp2)
+        {
+            if (coin(pcross))
+            {
+                double w = rd.NextDouble();
+                return w * fenp1 + (1.0 - w) * fenp2;
+            }
+            return fenp1;
+        }
+
+        //мутация вещ числа
+        private static double Mutation(double fen)
+        {
+            if (coin(pmut))
+            {
+                return fen + rd.NextDouble();
+            }
+            return fen;
+        }
+
+        //Генерация нового поколения
+        private static Indv[] Generation(Indv[] pop, bool check)
+        {
+            Indv[] pops = Selection(pop);
+            Indv[] newpop = Init(check);
+            int i = 0;
+            while (true)
+            {
+                //целочисленное кодирование
+                if (check)
+                {
+                    Crossover(newpop[i].chrom, newpop[i + 1].chrom,
+                                 pops[i].chrom, pops[i + 1].chrom);
+
+                    Mutation(newpop[i].chrom);
+                    Mutation(newpop[i + 1].chrom);
+
+                    newpop[i].fen = decode(newpop[i].chrom);
+                    newpop[i + 1].fen = decode(newpop[i + 1].chrom);
+                }
+                else //вещественное
+                {
+                    newpop[i].fen = Crossover(pops[i].fen, pops[i + 1].fen);
+                    newpop[i + 1].fen = Crossover(pops[i].fen, pops[i + 1].fen);
+                    newpop[i].fen = Mutation(newpop[i].fen);
+                    newpop[i + 1].fen = Mutation(newpop[i + 1].fen);
+                }
+
+                newpop[i].fit = f(newpop[i].fen);
+                newpop[i + 1].fit = f(newpop[i + 1].fen);
+
+                i += 2;
+                if (i >= popsize)
+                {
+                    break;
+                }
+            }
+            return newpop;
+        }
+
+        //создание случайной популяции
+        private static Indv[] Init(bool check)
+        {
+            List<Indv> newpop = new List<Indv>();
+            for (int i = 0; i < popsize; i++)
+            {
+                newpop.Add(new Indv(check));
+            }
+            return newpop.ToArray();
+        }
+
+        //получаем минимум фитнесс-функции
+        private static double GetMin(Indv[] pop)
+        {
+            double xm = pop[0].fen;
+            double fm = pop[0].fit;
+            for (int i = 0; i < popsize; i++)
+            {
+                if (fm > pop[i].fit)
+                {
+                    xm = pop[i].fen;
+                    fm = pop[i].fit;
+                }
+            }
+            return xm;
+        }
+
+        //поиск минимума функции
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //получаем сначала все параметры с формы
+            popsize = int.Parse(textBox1.Text);
+            pcross = GetContolText(textBox2);
+            pmut = GetContolText(textBox3);
+            epoch = int.Parse(textBox4.Text);
+            xmax = GetContolText(textBox6);
+            xmin = GetContolText(textBox7);
+
+            chart1.Series["f(x)"].Points.Clear();
+            chart1.Series["gmin"].Points.Clear();
+
+            //основной цикл по эпохам
+            Indv[] pop = Init(checkBox1.Checked);
+
+            double gmin = GetMin(pop);
+
+            //для построения графика функции
+            double x = 0;
+            chart1.Series["gmin"].Points.AddXY(Math.Round(gmin,3), f(gmin));
+
+            for (int i = 0; i < epoch; i++)
+            {
+                pop = Generation(pop, checkBox1.Checked);
+                gmin = GetMin(pop);
+                chart1.Series["gmin"].Points.AddXY(Math.Round(gmin,3), f(gmin));
+            }
+
+            //сам график
+            x = xmin;
+            double step = 1;
+            while (x <= xmax)
+            {
+                chart1.Series["f(x)"].Points.AddXY(Math.Round(x,3), f(x));
+                x += step;
+            }
+
+            textBox5.Text = "Xmin* = " + Math.Round(gmin,3).ToString() + "; Fmin* = " + Math.Round(f(gmin),3).ToString() + ";";
+        }
+
+        //получаем значения из формы заменяя при этом точку на запятую
+        public double GetContolText(TextBox textBox)
+        {
+            if (textBox.Text != "")
+            {
+                return Convert.ToDouble(textBox.Text.Replace(".", ","));
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
